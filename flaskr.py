38896df213ab0f1e6676e941a1d08e3944f3c4c4
Lev Maximov <lev.maximov@gmail.com>
2012-04-17 18:14:55 +0700

    tutorial completed

--- flaskr.py
+++ flaskr.py
@@ -35,8 +35,35 @@ def before_request():
 def teardown_request(exception):
     g.db.close()
 
+@app.route('/')
+def show_entries():
+    cur = g.db.execute('select title, text from entries order by id desc')
+    entries = [dict(title=row[0], text=row[1]) for row in cur.fetchall()]
+    return render_template('show_entries.html', entries=entries)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('show_entries'))
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('show_entries'))
+
+
 if __name__ == '__main__':
-    init_db()
-#    app.run()
+#    init_db()
+    app.run()
 
 
