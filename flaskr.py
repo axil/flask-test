144647f0cd4fb23876c3f4bb43079954633038eb
Lev Maximov <lev.maximov@gmail.com>
2012-04-17 17:12:51 +0700

    step 4

---
+++ flaskr.py
@@ -0,0 +1,42 @@
+# all the imports
+from __future__ import with_statement
+from contextlib import closing
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for,\
+     abort, render_template, flash
+
+# configuration
+DATABASE = 'flaskr.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+# create our little application :)
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+
+#print app.config['USERNAME']
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+
+@app.teardown_request
+def teardown_request(exception):
+    g.db.close()
+
+if __name__ == '__main__':
+    init_db()
+#    app.run()
+
+
